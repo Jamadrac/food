dacf2b34bb23c0ac25213c3d012f0fc148e2320c
rabbi kindalo <rabbikindalo001@gmail.com>
2022-11-20 12:17:25 +0200

    raiway

--- deliver/settings.py
+++ deliver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(b%y6my(@zizf(agz!*6j+picf^!%tfikl#8hk3@+=e!s4a8bo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
